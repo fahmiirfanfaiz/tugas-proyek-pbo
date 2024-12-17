c65b7f98d8e60c3d85cc543174f7a9b1e5e55fb2
fahmiirfanfaiz <fahmiirfanfaiz01@gmail.com>
2024-12-17 16:41:28 +0700

    feat: add calendar to UI

--- ToDoListApp/obj/Debug/net8.0-windows/ToDoListApp.AssemblyInfo.cs
+++ ToDoListApp/obj/Debug/net8.0-windows/ToDoListApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcb45d76dba8fc768834260891c773a1c3f173b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e4ec332a8ae355aae6d399515a480e840cd8079")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
