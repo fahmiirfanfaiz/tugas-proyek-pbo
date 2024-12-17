fe6e1475110478acfadafb3cd3fcf9bf0885418c
FAN2257 <145684738+FAN2257@users.noreply.github.com>
2024-12-17 20:27:11 +0700

    fix merge

--- TaskClass/obj/Debug/net8.0/TaskClass.AssemblyInfo.cs
+++ TaskClass/obj/Debug/net8.0/TaskClass.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskClass")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcb45d76dba8fc768834260891c773a1c3f173b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e4ec332a8ae355aae6d399515a480e840cd8079")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskClass")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskClass")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
