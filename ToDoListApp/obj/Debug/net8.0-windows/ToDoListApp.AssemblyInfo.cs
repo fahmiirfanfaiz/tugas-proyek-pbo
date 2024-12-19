ec064c6a8778ecd77c66041215f470dceb590469
FAN2257 <145684738+FAN2257@users.noreply.github.com>
2024-12-20 06:56:57 +0700

    fix merge

--- ToDoListApp/obj/Debug/net8.0-windows/ToDoListApp.AssemblyInfo.cs
+++ ToDoListApp/obj/Debug/net8.0-windows/ToDoListApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c30ac8aea40a3a9fb7a20c4a42d794b8f9e9f672")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9df1d84359aeae4c7174cde394b8bedaaa30fda4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
